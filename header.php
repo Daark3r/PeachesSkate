211692e24302ce80c467d401cff2afbb71f16309
Daark3r <luca.bazzea@gmail.com>
2018-06-08 16:16:49 +0200

    help from jason (created border around logo)

--- header.php
+++ header.php
@@ -3,32 +3,40 @@
 <head>
     <meta charset="utf-8">
   <title>Peaches Skateboarding</title>
-  <link rel="icon" href="popopop.png">
+  <link rel="icon" href="peach.png">
   <link rel="stylesheet" href="style.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
+  <header>
+    <div class="logo">
+      <img src="popopop.png" alt="peaches Skateboarding logo" width="auto"/>
+    </div>
 
     <div class="navbar">
-
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/peacheskateboarding/" target="_blank">
+            <div class="facebook">
+              <i class="fab fa-facebook"></i>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/peaches_skate/" target="_blank">
+            <div class="instagram">
+              <i class="fab fa-instagram"></i>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UCfj6NauHtF4zW_9-erliipQ" target="_blank">
+            <div class="youtube">
+              <i class="fab fa-youtube"></i>
+            </div>
+          </a>
+        </li>
+      </ul>
     </div>
-    <a href="https://www.facebook.com/peacheskateboarding/" target="_blank">
-      <div class="facebook">
-        <i class="fab fa-facebook"></i>
-      </div>
-    </a>
-    <a href="https://www.instagram.com/peaches_skate/" target="_blank">
-      <div class="instagram">
-        <i class="fab fa-instagram"></i>
-      </div>
-    </a>
-    <a href="https://www.youtube.com/channel/UCfj6NauHtF4zW_9-erliipQ" target="_blank">
-      <div class="youtube">
-        <i class="fab fa-youtube"></i>
-      </div>
-    </a>
-
-    <div class="logo">
-     <img src="popopop.png" alt="peaches Skateboarding logo" height="120px" width="auto"/>
-   </div>
-    <h1 class="align-left"></h1>
+  </header>
+  <h1 class="align-left"></h1>
