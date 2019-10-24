e6152d02ce8df217cc024a5b916d94ef6afc420e
LucaBazzea <luca.bazzea@gmail.com>
2019-10-24 12:29:38 +0200

    update contact form

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    $fname = $_POST['First Name'];
-    $lname = $_POST['Last Name'];
-    $mailFrom = $_POST['E-mail'];
-    $country = $_POST['Country'];
-    $message = $_POST['Message'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $mailFrom = $_POST['email'];
+    $country = $_POST['country'];
+    $message = $_POST['message'];
 
     $mailTo = "admin@moyacentre.co.za";
     $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $txt = "You have received an e-mail from ".$name.$country.".\n\n".$message;
 
 
 
